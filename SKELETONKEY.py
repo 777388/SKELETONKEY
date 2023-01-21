161c81f3456dfe914b98edc170d7da208d0ad741
777388 <96343159+777388@users.noreply.github.com>
2023-01-21 12:47:42 -0800

    Create SKELETONKEY.py

---
+++ SKELETONKEY.py
@@ -0,0 +1,22 @@
+import multiprocessing
+
+# Create a shared memory array to store the binary statement
+binary_statement = multiprocessing.Array('i', [0,0,0,0,0,0,0,0])
+
+def toggle_bit(thread_id):
+    while True:
+        binary_statement[thread_id] = 1 - binary_statement[thread_id]
+
+# Create the processes
+processes = []
+for i in range(8):
+    process = multiprocessing.Process(target=toggle_bit, args=(i,))
+    process.start()
+    processes.append(process)
+
+while True:
+    binary_string = ''.join(map(str, binary_statement))
+    character = chr(int(binary_string, 2))
+    return character
+    # Update the progress bar
+
