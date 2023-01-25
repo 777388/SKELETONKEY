5f11f6d3aa4346e5629e699f24b5305a161ab7dd
777388 <96343159+777388@users.noreply.github.com>
2023-01-25 13:57:37 -0800

    Update SKELETONKEY.py

--- SKELETONKEY.py
+++ SKELETONKEY.py
@@ -7,6 +7,7 @@ def toggle_bit(thread_id):
     while True:
         binary_statement[thread_id] = 1 - binary_statement[thread_id]
 
+
 # Create the processes
 processes = []
 for i in range(8):
@@ -14,8 +15,19 @@ for i in range(8):
     process.start()
     processes.append(process)
 
+def onechar():
+    while True:
+        binary_string = ''.join(map(str, binary_statement))
+        character = chr(int(binary_string, 2))
+        return character
+
+# Create an empty list to store the characters
+characters_list = []
 while True:
-    binary_string = ''.join(map(str, binary_statement))
-    character = chr(int(binary_string, 2))
-    # return the stream of characters
-    yield character
+    # Create a list of 8 characters
+    characters = [onechar() for _ in range(8)]
+    # Check if the characters already exist in the list
+    if ''.join(characters) not in characters_list:
+        characters_list.append(''.join(characters))
+        # Print the characters as a single string
+        print(''.join(characters))
